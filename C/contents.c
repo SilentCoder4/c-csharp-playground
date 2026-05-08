439c8ca8a1526f59eadf27b57aaa39c3f05e99fd
Ankit Halder <142447660+SilentCoder4@users.noreply.github.com>
2026-05-09 00:27:18 +0530

    Create contents.c

---
+++ C/contents.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main()  {
+    printf("\nBook Name: Let Us C\n");
+    printf("By Yashvant Kanetkar\n");
+    printf("Edition: 19th\n");
+    printf("==============================\n");
+    printf("1. Intruduction to C\n");
+    printf("2. C Instructions\n");
+    printf("3. Decision Control Instruction\n");
+    printf("4. More Complex Decision Making\n");
+    printf("5. Loops Control Instruction\n");
+    printf("6. More Complex Repetitions\n");
+    printf("7. Case Control Instruction\n");
+    printf("8. Function\n");
+    printf("9. Pointers\n");
+    printf("10. Recursion\n");
+    printf("11. Data Type Revisited\n");
+    printf("12. The C Preprocessor\n");
+    printf("13. Arrays\n");
+    printf("14. Multidimensional Arrays\n");
+    printf("15. Strings\n");
+    printf("16. Handling Multiple Strings\n");
+    printf("17. Structures\n");
+    printf("18. Console Input/Output\n");
+    printf("19. File Input/Output\n");
+    printf("20. More Issues Input/Output\n");
+    printf("21. Operations On Bits\n");
+    printf("22. Miscellaneous Features\n");
+    printf("23. Interview FAQ\n");
+    printf("24. The Next Level\n");
+    printf("==============================\n");
+
+}
